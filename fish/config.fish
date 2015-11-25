303ba60aff1d3f50c63175463e5d0e9eacb52465
David Barton <david.barton@posteo.de>
2015-11-25 12:16:43 +0100

    v3

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+# Add nvm-fish-wrapper
+source ~/.config/fish/nvm-wrapper/nvm.fish
+
+# Alias SBKS OpenVPN
+alias vpn='sudo openvpn --config /etc/openvpn/openvpn.cfg'
+
+# Set Glances alias
+alias stats='glances'
